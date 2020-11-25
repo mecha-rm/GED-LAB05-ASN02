36f680ffec2ec9b2f1fdc1ab37c304df5c1a44d9
Roderick Montague <roderick.montague243@gmail.com>
2020-11-25 14:06:02 -0500

    Basic Snow Shader Material
    Added a basic snow shader material. I also added a VFX shader, which will be for mist effects.

---
+++ Assets/Scripts/SnowShaderEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowShaderEditor : MonoBehaviour
+{
+    public Shader shader = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # GED-LAB05-ASN02
 The repository for individual assignment 2 for laboratory 5 of game engines.
+For this project snow and mist was used.
