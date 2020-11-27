b271cc203c3f9e456e18e9a61f67c829f4934d64
Roderick Montague <roderick.montague243@gmail.com>
2020-11-26 23:04:25 -0500

    Preparing for Recording
    Shaders have been added, so now I'm going to record a demonstration.

---
+++ Assets/Scripts/SnowShader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowShader : MonoBehaviour
+{
+    public Material material;
+    private Shader shader;
+    private Texture texture;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (material != null)
+        {
+            shader = material.shader;
+            texture = material.mainTexture;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
